bf940391cbb4a5638b3e152e2ed6e53a253cac10
coverj009 <win8yh1@outlook.com>
2023-08-24 00:05:07 +0800

    修改vant目录

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <view>123</view>
+  <view>
+    <van-tabbar :active="active" @change="tabbarChange">
+      <van-tabbar-item name="home" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item name="my" icon="user-circle-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </view>
 </template>
 
 <script setup>
+import { ref } from "vue";
+const active = ref("home");
+const tabbarChange = (event) => {
+  active.value = event.detail;
+};
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
